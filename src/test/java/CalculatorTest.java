151624ef3402183e113a0f252c0c5aef4cc750e1
maksym-huliaka <maxim.lviv1@gmail.com>
2020-01-23 14:19:44 +0200

    Rename class Calculation

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-class CalculationTest {
+class CalculatorTest {
 
     Calculator calculation = new Calculator();
 
